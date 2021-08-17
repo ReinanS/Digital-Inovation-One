2085b0ec8a2e9064c3d7b4433c9b54dc72e207bd
reinanS <reinandesouza01@gmail.com>
2021-08-17 07:46:00 -0300

    feat: example project

---
+++ Desenvolvimento básico em Java/caracteristicas-da-linguagem/src/test/java/one/inovation/digital/caracteristicasdalinguage/CaracteristicasDaLinguageApplicationTests.java
@@ -0,0 +1,13 @@
+package one.inovation.digital.caracteristicasdalinguage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaracteristicasDaLinguageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
