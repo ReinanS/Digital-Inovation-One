2d464862c1544a31f4991427656df1d29e40750d
reinanS <reinandesouza01@gmail.com>
2021-08-22 08:12:00 -0300

    feat: exercice 2

---
+++ Desenvolvimento básico em Java/trabalhandoComDatas/javaDate/Exercicio002-extra.java
@@ -0,0 +1,35 @@
+package aulas.trabalhandoComDatas.javaDate;
+
+import java.util.Date;
+
+public class Exercicio001 {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+    /**
+     * Exercicio 1 - extra
+     *
+     * 
+     * Descubra o timeInMillis do dia que você nasceu
+     * Converta em um objeto Date
+     * Verifique se ele é anterior ou posterior a 15 de maio de 2010
+   
+
+     */
+    
+    private boolean checkDate(Date date) {
+    
+    }
+
+    private long timeInMillis() {
+
+    }
+    
+   
+
+}
